edc81e79f82adcd7d8f3e39ef9c67360a0992597
Gordon Woon <gordonwoon@live.com>
2018-12-12 17:48:03 +0800

    docs

---
+++ docs/precache-manifest.728e4fa1e57293e72cca367bc6321a31.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "22e04a1ad29eb7b22166dddef2f77300",
+    "url": "./static/media/onetwo.22e04a1a.mp4"
+  },
+  {
+    "revision": "ad5588d753c80b568a01",
+    "url": "./static/css/main.27316ceb.chunk.css"
+  },
+  {
+    "revision": "2eab0d16fde2a7ec19f5b20757ace248",
+    "url": "./static/media/img_iridiscent.2eab0d16.jpg"
+  },
+  {
+    "revision": "3d3fbec0e89bc8f871fe",
+    "url": "./static/js/1.3d3fbec0.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "ad5588d753c80b568a01",
+    "url": "./static/js/main.ad5588d7.chunk.js"
+  },
+  {
+    "revision": "db8fdeaec12ba098bc6c609f3764f020",
+    "url": "./static/media/heartbeat.db8fdeae.mp3"
+  },
+  {
+    "revision": "e022c66d2a055d7d0886087bb319f2dd",
+    "url": "./static/media/PORTRAIT.e022c66d.jpg"
+  },
+  {
+    "revision": "b493b703a29a04b510461ac02e105fa7",
+    "url": "./static/media/educator.b493b703.mp4"
+  },
+  {
+    "revision": "3d3fbec0e89bc8f871fe",
+    "url": "./static/css/1.19d7eb1a.chunk.css"
+  },
+  {
+    "revision": "4642bf68f4d5e753f5ac73a676689754",
+    "url": "./index.html"
+  }
+];
