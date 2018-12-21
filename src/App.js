239818b6a67717586213997aecd2550d2ee53f8b
Eugene Ling <eugene@sunnysideup.sg>
2018-12-21 19:20:53 +0800

    page5

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Page1 } from './pages/Page1';
 import { Page2 } from './pages/Page2';
 import { Page3 } from './pages/Page3';
 import { Page4 } from './pages/Page4';
+import { Page5 } from './pages/Page5';
 import 'animate.css';
 import './styles/App.css';
 import './styles/Component.css';
@@ -16,6 +17,7 @@ class App extends React.Component {
           <Route path="/page2" exact component={Page2} />
           <Route path="/page3" exact component={Page3} />
           <Route path="/page4" exact component={Page4} />
+          <Route path="/page5" exact component={Page5} />
         </div>
       </Router>
     );
---
+++ src/pages/Page5.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { mountPageTween, unmountPageTween } from '../helper/tween';
+import Navigation from '../components/navigation';
+import PORTRAIT from '../assets/PORTRAIT.jpg';
+
+export class Page5 extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.pageContainer = React.createRef();
+  }
+
+  componentDidMount() {
+    mountPageTween(this.pageContainer.current);
+  }
+
+  handleNext = () => {
+    unmountPageTween(this.pageContainer.current);
+    setTimeout(() => {
+      this.props.history.push('/page4');
+    }, 1000)
+  }
+
+  render() {
+    return (
+      <div className="page-container" ref={this.pageContainer}>
+        <img className="img-potrait" src={PORTRAIT} alt="sampleImage" />
+      
+        <p className="text-potrait animated fadeIn">
+          <p><span>Age  </span>24</p>
+          <p><span>Qualification  </span>Engineering Degree</p>
+          <p><span>Work  </span>Construction Worker</p>
+          <p><span>Earns  </span>900/month</p>
+          <p><span>Paid  </span>10k for agency fees</p>
+          <p><span>Family needs  </span>500/month</p>
+        </p>
+      
+        <Navigation onNext={this.handleNext} />
+      </div>
+    );
+  }
+}
